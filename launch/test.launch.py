7cfe70f2dc5b169ed4c7785e4422c3b5edb4d940
NBauschmann <nathalie_bauschmann@hotmail.de>
2023-04-28 17:47:13 +0200

    work in progress

--- launch/test.launch.py
+++ launch/test.launch.py
@@ -8,16 +8,16 @@ def generate_launch_description():
     package_path = get_package_share_path(package_name)
     default_ekf_params_path = package_path / (
         'config/ekf_params.yaml')
-    mixer_launch_arg = launch.actions.DeclareLaunchArgument(
-        name='mixer_path',
+    ekf_launch_arg = launch.actions.DeclareLaunchArgument(
+        name='ekf_params_path',
         default_value=str(default_ekf_params_path),
         description='Path to the mixer configuration .yaml file.')
 
     return launch.LaunchDescription([
-        mixer_launch_arg,
+        ekf_launch_arg,
         launch_ros.actions.Node(
             package=package_name,
             executable='vision_ekf_node',
-            parameters=[launch.substitutions.LaunchConfiguration('mixer_path')],
+            parameters=[launch.substitutions.LaunchConfiguration('ekf_params_path')],
             output='screen'),
     ])
